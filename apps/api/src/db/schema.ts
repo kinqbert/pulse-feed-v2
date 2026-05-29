312e612207646fcbc07ab37fb745005943f756c3
kinqbert <perehodko1010@gmail.com>
2026-05-29 18:28:58 +0300

    feat: implement feed module with controller, service, and repository

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -1,17 +1,36 @@
+import { pgEnum } from "drizzle-orm/pg-core";
 import { timestamp } from "drizzle-orm/pg-core";
 import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
-export const activityTypeEnum = ["comment", "mention", "reaction"] as const;
+// ENUMS
+export const ActivityType = {
+  Comment: "comment",
+  Mention: "mention",
+  Reaction: "reaction",
+} as const;
 
+export type ActivityType = (typeof ActivityType)[keyof typeof ActivityType];
+
+// UTILS
+function enumToPgEnum<T extends Record<string, string>>(obj: T) {
+  return Object.values(obj) as [T[keyof T], ...T[keyof T][]];
+}
+
+// CORE TABLES
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
 });
 
+export const ActivityTypeEnum = pgEnum(
+  "activity_type_enum",
+  enumToPgEnum(ActivityType),
+);
+
 export const activities = pgTable("activities", {
   id: uuid("id").primaryKey().defaultRandom(),
-  type: text("type", { enum: activityTypeEnum }).notNull(),
+  type: ActivityTypeEnum().notNull(),
   actorId: uuid("actor_id")
     .notNull()
     .references(() => users.id),
